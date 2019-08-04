3d585518fcae431e686117f30250e4aab8746ed0
Area39 <Area39@163.com>
2019-08-03 20:16:55 -0400

    first commit

---
+++ webug/data/dbConfig.php
@@ -0,0 +1,16 @@
+<?php
+
+$host = 'localhost';
+$user = 'root';
+$pwd = 'toor';
+$database = 'webug';
+$port = 3306;
+
+$sysHost = 'localhost';
+$sysUser = 'root';
+$sysPwd = 'toor';
+$sysDatabase = 'webug_sys';
+$port = 3306;
+
+?>
+
